80513979cc969b9841bbff32d9ea11be7b90ab3f
Justin Fernando <jtafernando@gmail.com>
2023-11-19 20:16:24 -0800

    spinner-tutorial: added a spinner and a spinner adapter

--- app/src/main/java/com/example/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/example/androidfundamentals/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.androidfundamentals
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val monthsSpinner = findViewById<Spinner>(R.id.months_spinner)
+        val customList = listOf("First", "Second", "Third", "Fourth")
+        val spinnerAdapter = ArrayAdapter<String>(
+            this,
+            androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+            customList)
+        monthsSpinner.adapter = spinnerAdapter
+
+        monthsSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(
+                    this@MainActivity,
+                    "You selected ${parent?.getItemAtPosition(position).toString()}",
+                    Toast.LENGTH_LONG).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
     }
 }
